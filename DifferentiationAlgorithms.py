5cca6182c04268982afec716ff64f37866179914
Aleksandar Milovanović <59347685+aleksannndar@users.noreply.github.com>
2022-04-02 02:19:59 +0200

    added firstDerivativeFivePointApprox()

--- DifferentiationAlgorithms.py
+++ DifferentiationAlgorithms.py
@@ -2,10 +2,13 @@ import numpy as np
 from scipy.special import binom
 
 #Centered Difference Quotient
-def firstDerivativeAprox(func, x0, h):
+def firstDerivativeApprox(func, x0, h):
     return (func(x0 + h) - func(x0 - h)) / (2.0 * h)
 
-def derivativeAprox(func, x0, h, order):
+def firstDerivativeFivePointApprox(func, x0, h):
+    return (-func(x0 + 2*h) + 8*func(x0 + h) - 8*func(x0 - h) + func(x0 - 2*h)) / (12*h)
+
+def derivativeApprox(func, x0, h, order):
     if(order <= 0):
         raise Exception("Order of derivative has to be higher than 0")
     sum = 0
--- Playgroud.py
+++ Playgroud.py
@@ -3,4 +3,5 @@ import numpy as np
 
 
 print(np.cos(1))
-print(da.derivativeAprox(np.sin, 1, 0.0001, 3))
+print(da.firstDerivativeFivePointApprox(np.sin, 1, 0.001))
+print(da.firstDerivativeApprox(np.sin, 1, 0.001))

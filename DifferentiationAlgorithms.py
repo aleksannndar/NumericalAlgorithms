c3e27500c35e0b6d11df574f3ca161bed826b914
Aleksandar Milovanović <59347685+aleksannndar@users.noreply.github.com>
2022-04-02 02:13:49 +0200

    Added derivativeApprox()

--- DifferentiationAlgorithms.py
+++ DifferentiationAlgorithms.py
@@ -1,4 +1,15 @@
 import numpy as np
+from scipy.special import binom
 
+#Centered Difference Quotient
 def firstDerivativeAprox(func, x0, h):
     return (func(x0 + h) - func(x0 - h)) / (2.0 * h)
+
+def derivativeAprox(func, x0, h, order):
+    if(order <= 0):
+        raise Exception("Order of derivative has to be higher than 0")
+    sum = 0
+    for k in range(0, order+1):
+        sum+= (-1.0)**(k + order)*binom(order, k)*func(x0+k*h)
+    
+    return sum / (h**order)
--- Playgroud.py
+++ Playgroud.py
@@ -1,5 +1,6 @@
 import DifferentiationAlgorithms as da
 import numpy as np
 
+
 print(np.cos(1))
-print(da.firstDerivativeApprox(np.sin, 1 ,0.001))
+print(da.derivativeAprox(np.sin, 1, 0.0001, 3))
